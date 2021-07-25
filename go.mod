fd6115e88bb3ebb5845b5b047726154b88c618cb
Liren Zhu <liren.zhu.cn@gmail.com>
2021-07-25 09:27:56 -0700

    init commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/lirenzhucn/bookkeeper
+
+go 1.16
+
+require github.com/gorilla/mux v1.8.0
---
+++ internal/accounts.go
@@ -0,0 +1,26 @@
+package bookkeeper
+
+type Account struct {
+	Id       int     `json:"Id"`
+	Name     string  `json:"Name"`
+	Type     string  `json:"Type"`
+	Category string  `json:"Category"`
+	Balance  float32 `json:"Balance"`
+}
+
+var VALID_ACCOUNT_TYPES = []string{"Credit", "Debit"}
+var VALID_ACCOUNT_CATEGORIES = []string{"Asset", "Liability", "Revenue", "Expense"}
+
+func stringInList(s string, l []string) bool {
+	for _, ss := range l {
+		if s == ss {
+			return true
+		}
+	}
+	return false
+}
+
+func (account *Account) Validate() bool {
+	return stringInList(account.Type, VALID_ACCOUNT_TYPES) &&
+		stringInList(account.Category, VALID_ACCOUNT_CATEGORIES)
+}
