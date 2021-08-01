8a08930a7d16236806e002cb466b2ba73b1515a9
Liren Zhu <liren.zhu.cn@gmail.com>
2021-07-31 21:11:29 -0700

    set up logging in bkpctl main

--- cmd/bkpctl/main.go
+++ cmd/bkpctl/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/lirenzhucn/bookkeeper/internal/pkg/bkpctl/cmd"
+import (
+	"github.com/lirenzhucn/bookkeeper/internal/pkg/bkpctl/cmd"
+	"github.com/lirenzhucn/bookkeeper/internal/pkg/bookkeeper"
+)
 
 func main() {
+	bookkeeper.SetupZapGlobals()
 	cmd.Init()
 	cmd.Execute()
 }
