f9ad435e76e26ef74b38bc1a0f70cd73389a0875
Liren Zhu <liren.zhu.cn@gmail.com>
2021-08-04 11:30:48 -0700

    reporting api for account balance

---
+++ internal/pkg/api/reporting.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/lirenzhucn/bookkeeper/internal/pkg/bookkeeper"
+)
+
+type AccountWithBalance struct {
+	bookkeeper.Account
+	Balance int64 `json:"Balance"`
+}
+
+func getAccountBalanceOn(w http.ResponseWriter, r *http.Request) {
+	accountName := r.FormValue("accountName")
+	dateStr := r.FormValue("date")
+	if accountName == "" || dateStr == "" {
+		http.Error(w, "Invalid query string", 400)
+		return
+	}
+	date, err := time.Parse("2006/01/02", dateStr)
+	if err != nil {
+		http.Error(w, "Invalid query term date", 400)
+		return
+	}
+	offset, _ := time.ParseDuration("23h59m59s")
+	date = date.Add(offset)
+	account, balance, err := bookkeeper.ComputeAccountBalance(dbpool, accountName, date)
+	if !checkErr(err, w, 500, "Failed to query account balance",
+		"accountName", accountName) {
+		return
+	}
+	account_ := AccountWithBalance{account, balance}
+	json.NewEncoder(w).Encode(account_)
+}
