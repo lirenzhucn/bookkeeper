ba84ef582a88112f7c1cbda754683533b3159a4d
Liren Zhu <liren.zhu.cn@gmail.com>
2021-08-21 07:08:17 -0700

    move report schema to separate files

--- internal/pkg/bkpctl/cmd/report.go
+++ internal/pkg/bkpctl/cmd/report.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
@@ -45,52 +46,7 @@ func initReportCmd(rootCmd *cobra.Command) {
 		"Specify a list of liability tags to report separately",
 	)
 	balanceCmd.Flags().StringP(
-		"report-schema", "r",
-		`{
-"mapping": {
-	"Cash or Equivalent": ["Assets/cash"],
-	"Taxable Securities": ["Assets/taxable+liquid"],
-	"Liquid Assets": ["Assets/cash", "Assets/taxable+liquid"],
-	"Retirement Savings": ["Assets/retirement"],
-	"Education Savings": ["Assets/education"],
-	"Other Nonliquid": ["Assets/nonliquid"],
-	"Assets excl. Home": [
-		"Assets/cash", "Assets/taxable+liquid", "Assets/retirement", "Assets/education", "Assets/nonliquid"
-	],
-	"Home Net Mortgage": ["Assets/real estate"],
-	"Total Assets": ["Assets/TOTAL"],
-	"Short Term Liabilities": ["Liabilities/credit card"],
-	"Long Term Liabilities": ["Liabilities/loan"],
-	"Total Liabilities": ["Liabilities/TOTAL"],
-	"Stockholders Equities": ["Equities/TOTAL"]
-},
-"order": [
-	"Cash or Equivalent",
-	"Taxable Securities",
-	"Liquid Assets",
-	"-",
-	"Retirement Savings",
-	"Education Savings",
-	"Other Nonliquid",
-	"Assets excl. Home",
-	"-",
-	"Home Net Mortgage",
-	"Total Assets",
-	"-",
-	"Short Term Liabilities",
-	"Long Term Liabilities",
-	"Total Liabilities",
-	"-",
-	"Stockholders Equities"
-],
-"formatters": {
-	"Liquid Assets": ["bold"],
-	"Assets excl. Home": ["bold"],
-	"Total Assets": ["bold", "green"],
-	"Total Liabilities": ["bold", "red"],
-	"Stockholders Equities": ["bold", "yellow"]
-}
-}`,
+		"report-schema", "r", "configs/tpl/balance_sheet_tpl.json",
 		"Specify a report schema using a JSON string",
 	)
 	balanceCmd.Flags().StringP("date", "d", "",
@@ -128,78 +84,7 @@ func initReportCmd(rootCmd *cobra.Command) {
 		"Specify matchers for Investments",
 	)
 	incomeCmd.Flags().String(
-		"report-schema",
-		`{
-"mapping": {
-	"Salaries": ["Revenue/Professional Income/Salary"],
-	"RSU": ["Revenue/Professional Income/RSU"],
-	"Employer Match": ["Revenue/Professional Income/Employer Match"],
-	"Other Revenue": ["Revenue/Other Income/"],
-	"Total Revenue": ["Revenue/TOTAL"],
-	"Tax Expenses": ["Taxes/TOTAL"],
-	"Revenue Net Taxes": ["RevenueNetTaxes/TOTAL"],
-	"Mortgage Interests": ["Expenses/Home/Mortgage Interest"],
-	"Loan Fees": ["Expenses/Home/Loan Fees"],
-	"HOA": ["Expenses/Home/HOA"],
-	"Food & Dining": ["Expenses/Food & Dining/"],
-	"Kids": ["Expenses/Kids/"],
-	"Bills & Utilities": ["Expenses/Bills & Utilities/"],
-	"Transportation": ["Expenses/Transportation/"],
-	"Entertainment": ["Expenses/Entertainment/"],
-	"Shopping": ["Expenses/Shopping/"],
-	"Communications": ["Expenses/Communications/"],
-	"Medical Exp": ["Expenses/Medical Exp/"],
-	"Other Exp": ["Expenses/Other Exp/"],
-	"Total Expenses": ["Expenses/TOTAL"],
-	"Operating Income": ["OperatingIncome/TOTAL"],
-	"Taxable Investments": ["Investments/Investment/Taxable Investment"],
-	"Retirement Investments": ["Investments/Investment/Retirement Investment"],
-	"Education Investments": ["Investments/Investment/Education Investment"],
-	"Invest Gains/Losses": ["Investments/TOTAL"],
-	"Total Earnings": ["TotalEarnings/TOTAL"]
-},
-"order": [
-	"Salaries",
-	"RSU",
-	"Employer Match",
-	"Other Revenue",
-	"Total Revenue",
-	"-",
-	"Tax Expenses",
-	"Revenue Net Taxes",
-	"-",
-	"Mortgage Interests",
-	"Loan Fees",
-	"HOA",
-	"Food & Dining",
-	"Kids",
-	"Bills & Utilities",
-	"Transportation",
-	"Entertainment",
-	"Shopping",
-	"Communications",
-	"Medical Exp",
-	"Other Exp",
-	"Total Expenses",
-	"-",
-	"Operating Income",
-	"-",
-	"Taxable Investments",
-	"Retirement Investments",
-	"Education Investments",
-	"Invest Gains/Losses",
-	"-",
-	"Total Earnings"
-],
-"formatters": {
-	"Total Revenue": ["bold", "green"],
-	"Revenue Net Taxes": ["bold", "yellow"],
-	"Total Expenses": ["bold", "red"],
-	"Operating Income": ["bold", "green", "underline"],
-	"Invest Gains/Losses": ["bold", "green", "underline"],
-	"Total Earnings": ["bold", "yellow", "underline"]
-}
-}`,
+		"report-schema", "configs/tpl/income_statement_tpl.json",
 		"Specify a report schema using a JSON string",
 	)
 	reportCmd.AddCommand(balanceCmd)
@@ -213,6 +98,24 @@ type ReportSchema struct {
 	Formatters map[string][]string `json:"formatters"`
 }
 
+func readReportSchema(p string) (r ReportSchema, err error) {
+	var (
+		f *os.File
+		b []byte
+	)
+	f, err = os.Open(p)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	b, err = ioutil.ReadAll(f)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(b, &r)
+	return
+}
+
 func generateBalanceSheet(cmd *cobra.Command, args []string) {
 	assetTags, err := cmd.Flags().GetStringSlice("asset-tags")
 	cobra.CheckErr(err)
@@ -222,10 +125,9 @@ func generateBalanceSheet(cmd *cobra.Command, args []string) {
 	if dateStr == "" {
 		dateStr = time.Now().Format("2006/01/02")
 	}
-	reportSchemaStr, err := cmd.Flags().GetString("report-schema")
+	reportSchemaPath, err := cmd.Flags().GetString("report-schema")
 	cobra.CheckErr(err)
-	var reportSchema ReportSchema
-	err = json.Unmarshal([]byte(reportSchemaStr), &reportSchema)
+	reportSchema, err := readReportSchema(reportSchemaPath)
 	cobra.CheckErr(err)
 
 	url_ := fmt.Sprintf(
@@ -345,10 +247,9 @@ func generateIncomeStatement(cmd *cobra.Command, args []string) {
 	cobra.CheckErr(err)
 	investmentsTags, err := cmd.Flags().GetStringSlice("investments-tags")
 	cobra.CheckErr(err)
-	reportSchemaStr, err := cmd.Flags().GetString("report-schema")
+	reportSchemaPath, err := cmd.Flags().GetString("report-schema")
 	cobra.CheckErr(err)
-	var reportSchema ReportSchema
-	err = json.Unmarshal([]byte(reportSchemaStr), &reportSchema)
+	reportSchema, err := readReportSchema(reportSchemaPath)
 	cobra.CheckErr(err)
 
 	url_ := fmt.Sprintf(
